5990230f0ddf2d7efb7ff382705eca89deef7357
Kenji <kjadachi2@gmail.com>
2020-02-22 20:06:29 -0800

    Initial Commit

---
+++ CougBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CougBoard
+//
+//  Created by CrimsinCode2020 on 2/22/20.
+//  Copyright © 2020 CrimsinCode2020. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
